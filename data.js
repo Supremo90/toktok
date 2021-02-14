4d9582bd7348c1b2d786a13f1a697ae93583c945
Supremo90 <78866576+Supremo90@users.noreply.github.com>
2021-02-14 15:08:31 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = [{
+    url:"https://images.unsplash.com/photo-1526512340740-9217d0159da9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dmVydGljYWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    likes: "234",
+},{
+    url:"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    likes: "174",
+},{
+    url:"https://www.gettyimages.it/gi-resources/images/500px/983794168.jpg",
+    likes: "237",
+}];
+
+module.exports = data;
